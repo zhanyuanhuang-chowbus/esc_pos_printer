3e01e615ca63b0466002d1c206b3d74efcec1949
zhanyuanhuang-chowbus <zhanyuan.huang@chowbus.com>
2021-10-25 09:21:30 +0800

    add log

--- lib/src/network_printer.dart
+++ lib/src/network_printer.dart
@@ -43,7 +43,8 @@ class NetworkPrinter {
         print('Received from printer : ${String.fromCharCodes(event)}');
       });
       Timer.periodic(Duration(milliseconds: 100), (timer) {
-        _socket.add('\x10\x04\x02'.codeUnits);
+        _socket.add(_generator.rawBytes('\x10\x04\x02'.codeUnits));
+        // _socket.add('\x10\x04\x02'.codeUnits);
       });
       _socket.add(_generator.reset());
       return Future<PosPrintResult>.value(PosPrintResult.success);
